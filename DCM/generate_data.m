8d133354c1476692f7b38700293dc9960058c504
Samson Bankole <tsbankole@mix.wvu.edu>
2018-02-27 08:47:03 -0500

    Update generate_data.m

--- DCM/generate_data.m
+++ DCM/generate_data.m
@@ -1,4 +1,6 @@
-clear
+% to generate data for dcm
+clear all
+close all
 Cas= 3;
 Cbs = 1.117;
 f_ov_ss =4/7;
@@ -7,11 +9,12 @@ tspan = [0 10];
 interval = 2;
 Ca0 = 2;
 Cb0 = 1.117;
-y0=[Ca0;Cb0;Ca0;Cb0];
+y0=[Ca0;Cb0;Cas;Cbs];
 f_OV = [0 0.5];
 [t2,y,f_ov_ss, Rvss,u] = vandevusse_Nonlinear_separator(tspan,f_OV,y0);
 t_sp = [tspan(1):0.05:tspan(2)]';
-y_sp = interp1(t2,y,t_sp,'spline');
-y_sp = y_sp' - repmat([Cas; Cbs; Cas; Cbs], [1 size(y_sp,1)]);
+y_sp = interp1(t2,y,t_sp,'spline') - repmat([Cas Cbs Cas Cbs], [length(t_sp) 1]);
+u_sp = interp1(t2,u,t_sp,'next') - repmat([f_ov_ss, Rvss], [length(t_sp) 1]);
+u_sp = u_sp';
+y_sp = y_sp';
 t_sp = t_sp';
-clc
